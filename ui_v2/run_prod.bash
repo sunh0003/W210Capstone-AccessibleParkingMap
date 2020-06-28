ca67da027dc353fe0778f7660e3b7dc0f660d214
rachael <rburns@hioscar.com>
2020-06-28 14:57:44 -0400

    modifying for vm

--- ui_v2/run_prod.bash
+++ ui_v2/run_prod.bash
@@ -1,13 +1,13 @@
 #!/bin/bash
-docker kill flask_prod
-docker kill react_prod
+sudo docker kill flask_prod
+sudo docker kill react_prod
 
 cd frontend
-docker build -f Dockerfile.prod -t react:prod .
-docker run -it --name react_prod --rm -p 3001:80 react:prod
+sudo docker build -f Dockerfile.prod -t react:prod .
+sudo docker run -d --name react_prod --rm -p 3001:80 react:prod
 
 cd ../middleware
-docker build -f Dockerfile.prod -t flask:prod .
-docker run -d  --name flask_prod --rm  -p 5000:5000 flask:latest
+sudo docker build -f Dockerfile.prod -t flask:prod .
+sudo docker run -d  --name flask_prod --rm  -p 5000:5000 flask:latest
 
 
