7fbf3c151b388b8f60a3ee036fa42becb0f88054
rachael <rburns@hioscar.com>
2020-06-28 15:02:55 -0400

    fix container name

--- ui_v2/run_prod.bash
+++ ui_v2/run_prod.bash
@@ -8,6 +8,6 @@ sudo docker run -d --name react_prod --rm -p 3001:80 react:prod
 
 cd ../middleware
 sudo docker build -f Dockerfile.prod -t flask:prod .
-sudo docker run -d  --name flask_prod --rm  -p 5000:5000 flask:latest
+sudo docker run -d  --name flask_prod --rm  -p 5000:5000 flask:prod
 
 
